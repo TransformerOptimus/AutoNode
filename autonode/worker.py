41d38c565350fb1dd79ba5b2eb8c7e5c6746bdc5
MananX <Singhmanandeep81@gmail.com>
2024-05-27 15:57:08 +0530

    worker and setup files

---
+++ autonode/worker.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import traceback
+
+from autonode.logger.logger import logger
+# import logger
+from autonode.services.autonode import AutonodeService
+from celery import Celery
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from asgi_correlation_id.extensions.celery import load_correlation_ids
+
+from autonode.models.requests import Requests
+from autonode.utils.enums.request_status import RequestStatus
+
+load_dotenv()
+REDIS_URL = os.getenv("REDIS_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+
+# logger = logger.getLogger(__name__)
+
+
+def connect_db():
+    engine = create_engine(
+        DB_URL,
+        pool_size=20,  # Maximum number of database connections in the pool
+        max_overflow=50,  # Maximum number of connections that can be created beyond the pool_size
+        pool_timeout=30,  # Timeout value in seconds for acquiring a connection from the pool
+        pool_recycle=1800,  # Recycle connections after this number of seconds (optional)
+        pool_pre_ping=False,  # Enable connection health checks (optional)
+    )
+    connection = engine.connect()
+    connection.close()
+    return engine
+
+
+celery_app = Celery(
+    "worker",
+    broker=REDIS_URL,
+    backend=REDIS_URL,
+    # include=["jobs.test_job"],
+    # imports=["worker"],
+)
+
+
+def create_session():
+    engine = connect_db()
+    session = sessionmaker(bind=engine)()
+    return session
+
+
+load_correlation_ids()
+
+
+@celery_app.task
+def initiate_autonode(
+        objective: str,
+        screenshots_dir: str,
+        url: str,
+        graph_path: str,
+        request_id: int,
+        root_node: str = "1",
+):
+    session = create_session()
+    logger.info(f"Initiating Autonode for request {request_id}")
+    try:
+        driver = AutonodeService(
+            objective=objective, graph_path=graph_path, root_node=root_node
+        )
+        driver.run(session=session, request_id=request_id, url=url, request_dir=screenshots_dir)
+    except Exception as e:
+        Requests.update_request_status(session=session, request_id=request_id, status=RequestStatus.FAILED.value)
+        logger.error(f"Error: {str(e)}, traceback : {traceback.print_exc()}")
+    finally:
+        if os.path.exists(screenshots_dir):
+            logger.info(f"Deleting request directory {screenshots_dir}")
+            shutil.rmtree(screenshots_dir)
+        session.close()
