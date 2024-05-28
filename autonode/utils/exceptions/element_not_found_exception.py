cb675a2b99d5375ff510fc11eb92b41836cc206a
MananX <Singhmanandeep81@gmail.com>
2024-05-28 19:23:27 +0530

    Custom Exception Retry Fix

--- autonode/utils/exceptions/element_not_found_exception.py
+++ autonode/utils/exceptions/element_not_found_exception.py
@@ -1,5 +1,4 @@
 class ElementNotFoundException(Exception):
     def __init__(self, request_id, element):
-        self.message = (f"Element {element} not found for request_id: {request_id} after 5 retries."
-                        f"Retrying the task now...")
+        self.message = f"Element {element} not found for request_id: {request_id} after 5 retries."
         super().__init__(self.message)
--- autonode/utils/exceptions/llm_objective_exception.py
+++ autonode/utils/exceptions/llm_objective_exception.py
@@ -1,6 +1,5 @@
 class LLMObjectiveException(Exception):
     def __init__(self, request_id):
-        self.message = (f"Received Null Objective from llm for request_id: {request_id}."
-                        f" Retrying the task now...")
+        self.message = f"Received Null Objective from llm for request_id: {request_id}."
         super().__init__(self.message)
         
